0fd7887bae59099ce76c7c3c07283fed0cf998f3
beerappabvgp <beerappabharathb@gmail.com>
2024-05-20 16:31:39 +0530

    completed basics of strings

---
+++ strings.js
@@ -0,0 +1,27 @@
+// Strings
+
+const quote = "A mad passion for programming";
+console.log(quote);
+
+const useSingleQuote = "Use single quotes to declare and initialize strings";
+console.log(useSingleQuote);
+
+
+// template literals 
+const useBackTicks = `They give you super powers`;
+console.log(useBackTicks);
+
+// embed js in template literals 
+
+const greeting = "Welcome to my space";
+console.log(`This is my way of greeting ${greeting}`);
+
+
+// concatenation using + operator
+
+const firstPart = "Good";
+const secondPart = "Average";
+const film = firstPart + " + " + secondPart;
+console.log(film);
+    
+
